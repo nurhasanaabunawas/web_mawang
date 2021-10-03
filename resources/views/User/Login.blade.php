728878260d93f5183946308ad6d63e9aeba9e5e6
caca <nurhasanahabunawas@gmail.com>
2021-10-04 02:03:58 +0800

    edit login page

--- resources/views/User/Login.blade.php
+++ resources/views/User/Login.blade.php
@@ -16,21 +16,28 @@
 </head>
 <body class="hold-transition login-page">
 <div class="login-box">
+    
+<a  class="brand-link">
+    <center>
+      <img src="{{ URL::asset('assets/dist/img/gowa.png') }}" alt="AdminLTE Logo" width="70" >
+      </center>
+    </a>
   <div class="login-logo">
-    <a><b>Admin</b>LTE</a>
+        
+    <a><b>Kelurahan Mawang</b></a>
   </div>
   <!-- /.login-logo -->
   <div class="card">
     <div class="card-body login-card-body">
-      <p class="login-box-msg">Sign in to start your session</p>
+      <p class="login-box-msg">Silahkan Login</p>
 
       <form action="{{route('postlogin')}}" method="post">
           {{ csrf_field() }}
         <div class="input-group mb-3">
-          <input type="email" class="form-control" name="email" placeholder="Email">
+          <input type="email" class="form-control" name="email" placeholder="Nama">
           <div class="input-group-append">
             <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
+              <span class="fas fa-pencil-alt"></span>
             </div>
           </div>
         </div>
@@ -43,39 +50,14 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-8">
+          <div class="col 8">
             <div class="icheck-primary">
-              <input type="checkbox" id="remember">
-              <label for="remember">
-                Remember Me
-              </label>
+            <button type="submit" class="btn btn-primary btn-block">Login</button>
             </div>
           </div>
-          <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-          </div>
-          <!-- /.col -->
         </div>
       </form>
 
-      <div class="social-auth-links text-center mb-3">
-        <p>- OR -</p>
-        <a href="#" class="btn btn-block btn-primary">
-          <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-        </a>
-        <a href="#" class="btn btn-block btn-danger">
-          <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-        </a>
-      </div>
-      <!-- /.social-auth-links -->
-
-      <p class="mb-1">
-        <a href="forgot-password.html">I forgot my password</a>
-      </p>
-      <p class="mb-0">
-        <a href="register.html" class="text-center">Register a new membership</a>
-      </p>
     </div>
     <!-- /.login-card-body -->
   </div>
