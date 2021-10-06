22fc1e9c37c633144526bf8d0886971b238bf86c
fadilahrmdhn <fadilahramadhani1712@gmail.com>
2021-10-06 10:54:09 +0800

    Add Data UMKM

--- resources/views/User/lembaga/fkpm/index.blade.php
+++ resources/views/User/lembaga/fkpm/index.blade.php
@@ -149,35 +149,19 @@
                             <div class="entry-footer">
                                 <i class="bi bi-folder"></i>
                                 <ul class="cats">
-                                    <li><a href="#">Business</a></li>
+                                    <li><a href="#">Public Safety</a></li>
                                 </ul>
 
                                 <i class="bi bi-tags"></i>
                                 <ul class="tags">
-                                    <li><a href="#">Creative</a></li>
-                                    <li><a href="#">Tips</a></li>
-                                    <li><a href="#">Marketing</a></li>
+                                    <li><a href="#">Cooperation</a></li>
+                                    <li><a href="#">Society</a></li>
+                                    <li><a href="#">Police</a></li>
                                 </ul>
                             </div>
 
                         </article>
                         <!-- End blog entry -->
-
-                        <div class="blog-author d-flex align-items-center">
-                            <img src="{{ URL::asset('assets/user/assets/img/blog/blog-author.jpg') }}" class="rounded-circle float-left" alt="">
-                            <div>
-                                <h4>Jane Smith</h4>
-                                <div class="social-links">
-                                    <a href="https://twitters.com/#"><i class="bi bi-twitter"></i></a>
-                                    <a href="https://facebook.com/#"><i class="bi bi-facebook"></i></a>
-                                    <a href="https://instagram.com/#"><i class="biu bi-instagram"></i></a>
-                                </div>
-                                <p>
-                                    Itaque quidem optio quia voluptatibus dolorem dolor. Modi eum sed possimus accusantium. Quas repellat voluptatem officia numquam sint aspernatur voluptas. Esse et accusantium ut unde voluptas.
-                                </p>
-                            </div>
-                        </div>
-                        <!-- End blog author bio -->
 
                     </div>
 
